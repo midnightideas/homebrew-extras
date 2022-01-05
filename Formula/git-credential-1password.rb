b24e4fef638d835c8c5326be1824a428306cc332
D Lio <d@derycka.com>
2022-01-05 08:26:01 +0000

    Update git-credential-1password

--- Formula/git-credential-1password.rb
+++ Formula/git-credential-1password.rb
@@ -2,9 +2,11 @@ class GitCredential1password < Formula
   desc '1Password Git Credential Helper'
   homepage 'https://github.com/midnightideas/git-credential-1password'
   head 'https://github.com/midnightideas/git-credential-1password.git'
-  url 'https://github.com/midnightideas/git-credential-1password/releases/download/v1.1.1/git-credential-1password-v1.1.1.tar.gz'
-  sha256 '8dec65d2f50915f83407a4a61cee82ab55f734d801eca228a48b0113b217cfe4'
+  url 'https://github.com/midnightideas/git-credential-1password/releases/download/v1.2.0/git-credential-1password-v1.2.0.tar.gz'
+  sha256 'f15106d410c491cfc12bb0a720a6314fcec4073bd498419fb3f91d37b58fc9aa'
   license 'MIT'
+
+  depends_on 'jq'
 
   def install
     bin.install 'git-credential-1password'
